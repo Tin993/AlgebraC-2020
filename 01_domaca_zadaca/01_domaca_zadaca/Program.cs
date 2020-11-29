c4317ebd3e84398a783b2ea295aa22e3f428b747
Tin Špiljak <spiljak7tin@gmail.com>
2020-11-29 19:47:57 +0100

    Domaca zadaca

---
+++ 01_domaca_zadaca/01_domaca_zadaca/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01_domaca_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+        }
+    }
+}
---
+++ 01_domaca_zadaca/Umnozak i kvocijent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Umnozak_i_kvocijent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Unos brojeva
+            int a;
+            Console.WriteLine("Upišite prvi cijeli broj");
+            a = int.Parse(Console.ReadLine());
+
+            int b;
+            Console.WriteLine("Upišite drugi cijeli broj");
+            b = int.Parse(Console.ReadLine());
+
+            //Kvocijent i umonzak
+            double Kvocijent;
+            Kvocijent = (double)a / (double)b;
+            Console.WriteLine("Kvocijent je {0}", Kvocijent);
+
+            int Umnozak;
+            Umnozak = a * b;
+            Console.WriteLine("Umnozak je {0}", Umnozak);
+
+        }
+    }
+}
